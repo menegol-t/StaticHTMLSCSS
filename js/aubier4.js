157be8b164d4c66f48921ae67e5cf45e33bc26f8
tom <tomas.menegol@gmail.com>
2022-06-24 13:20:43 -0300

    codigo para json en productos

--- js/aubier4.js
+++ js/aubier4.js
@@ -1,3 +1,31 @@
+//COMIENZA CARDS DE PRODUCTOS 
+
+const divProductos = document.getElementById("divProductos")
+
+fetch("./productos.json")
+    .then( (anteojos) => anteojos.json() )
+    .then((lentes) => {
+
+    lentes.forEach((anteojo) => {
+        const div = document.createElement("div")
+        div.innerHTML = `
+        <article class="col-md-6">
+            <figure class="width2" id="${anteojo.figure}">
+                <img src="${anteojo.src}" alt="${anteojo.alt}" class="scale2 img-fluid">
+                <figcaption class="txtCenter mt-3 fontSpecial txtMed">${anteojo.nombre}<br> ${anteojo.precio}</figcaption>
+                <div class="text-center bgNormal">
+                    <div class="card-body noneBorder">
+                        <button id="${anteojo.id}" type="button" class="comprarbtn btn btn-outline-light w-25 boton__responsive"> <span class="txtSmall2 " >Comprar</span></button>
+                    </div>
+                </div>
+            </figure>
+        </article>
+        `
+        divProductos.append(div)
+    }
+)})
+
+//COMIENZA FORMULARIO NOVEDADES
 let botonEnviar = document.getElementById("botonEnviar")
 let titulo = document.getElementById("titulo")
 let mensaje
@@ -85,3 +113,39 @@ function registrarDatos(nombre, apellido, email){
     }
 }/*Crea el nuevo usuario. Verifica que ya se haya creado el array en LocalStorage. 
 Si ya estaba creado, pushea el usuario. Si no estaba creado, lo crea y pushea el usuario.*/ 
+
+//COMIENZA TOAST DE BOTON DE COMPRA
+let dijonbtn = document.getElementById("dijonbtn")
+let lemansbtn = document.getElementById("lemansbtn")
+let lyonbtn = document.getElementById("lyonbtn")
+let nantesbtn = document.getElementById("nantesbtn")
+
+function toast(){
+    Toastify({
+        text: "Añadido al carrito!",
+        duration: 3000,
+        gravity: "bottom",
+        style: {
+            background: "#E0A367",
+          }
+    }).showToast();
+}
+
+dijonbtn.addEventListener("click", toast)
+lemansbtn.addEventListener("click", toast)
+lyonbtn.addEventListener("click", toast)
+nantesbtn.addEventListener("click", toast)
+
+/*
+<article class="col-md-6">
+	<figure class="width2" id="figureDijon">
+		<img src="../img/dijon/dijon.jpg" alt="Anteojos de lente circular, madera rayada en rojo y negro." class="scale2 img-fluid">
+	    <figcaption class="txtCenter mt-3 fontSpecial txtMed">Dijon <br> $7.500,00</figcaption>
+		<div class="text-center bgNormal">
+			<div class="card-body noneBorder">
+				<button id="dijonbtn" type="button" class="comprarbtn btn btn-outline-light w-25 boton__responsive"> <span class="txtSmall2 " >Comprar</span></button>
+			</div>
+		</div>
+	</figure>
+</article>
+*/
